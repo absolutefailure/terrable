354f6419cc304cc89c2410e08f320c2670cd2bf1
Shatteri <elias.isopahkala@gmail.com>
2023-02-09 16:15:14 +0200

    AUGH

--- terrable/core/src/com/mygdx/game/map/Map.java
+++ terrable/core/src/com/mygdx/game/map/Map.java
@@ -28,10 +28,10 @@ public class Map {
         this.mapSizeY = sizeY;
 
         
-        mudTexture = new Texture("mud3.png");
-        grassTexture = new Texture("grass3.png");
+        mudTexture = new Texture("mud.png");
+        grassTexture = new Texture("grass.png");
         woodTexture = new Texture("wood.png");
-        leavesTexture = new Texture("leaves2.png");
+        leavesTexture = new Texture("leaves.png");
         ladderTexture = new Texture("ladder.png");
         stoneTexture = new Texture("stone.png");
 
